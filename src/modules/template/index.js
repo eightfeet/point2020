1e079a247b8eb2e6e112366584018bb43747406c
肖吉宁 <eightfeet@xiaojiningdeMacBook-Pro.local>
2020-04-22 07:45:09 +0800

    update

--- src/modules/template/index.js
+++ src/modules/template/index.js
@@ -17,7 +17,7 @@ export default (disabledPhone, verifyPhone, hidePhone, data, buttonText, element
 	const { phone, verificationCode, antiFakeCode, scan, sendVerificationCode, submit} = elementNodeMappingField;
 	return `<div class="${prefix}-wrap" style="font-size:${is_mobi ? (31.25 * (width / 750)) : 13}px">
         ${hidePhone ? '' : `<div class="${prefix}-phone-item">
-            <input class="${prefix}-phone" type="tel" value="${data.phone || ''}" ${disabledPhone ? 'disabled' : ''} id="${phone}" maxlength="11" placeholder="输入手机号码" />
+            <input class="${prefix}-phone" type="tel" value="${data.phone || ''}" ${disabledPhone ? 'disabled' : ''} id="${phone}" maxlength="11" placeholder="输入手机号码" /><span class="${prefix}-phone-icon"></span>
         </div>`}
         ${verifyPhone ? `<div class="${prefix}-verificationcode-item">
             <input class="${prefix}-verificationcode" type="tel" id="${verificationCode}"  value="${data.verificationCode || ''}" maxlength="4" placeholder="输入验证码" />
